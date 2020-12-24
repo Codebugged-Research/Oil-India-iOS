e1973916e80d3631c6740b88e3809e597f31c193
sayannath <sayannath235@gmail.com>
2020-12-24 18:23:03 +0530

    push

---
+++ App.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+import { WebView } from 'react-native-webview';
+import { BackHandler, Platform } from "react-native"
+
+const App = () => {
+  const webView = useRef(null);
+  const [canGoBack, setCanGoBack] = useState(false)
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      BackHandler.addEventListener('hardwareBackPress', HandleBackPressed);
+
+      return () => {
+        BackHandler.removeEventListener('hardwareBackPress', HandleBackPressed);
+      }
+    }
+  }, []);
+  const HandleBackPressed = () => {
+    if (webView.current) {
+      webView.current.goBack();
+      return true; // PREVENT DEFAULT BEHAVIOUR (EXITING THE APP)
+    }
+    return false;
+  }
+
+  return (
+    <>
+      <WebView
+      ref={webView}
+        source={{
+          uri: 'https://www.oilcp2.com'
+        }}
+        onError={ ()=>{ webView.reload(); } }
+        onNavigationStateChange={navState => setCanGoBack(navState.canGoBack)}
+      />
+    </>
+  );
+};
+
+export default App;
